ae6210a1c9a5b2b82fc870c6469aa5da4fea31a3
saifulalamfahime <you@example.com>
2023-03-02 15:18:00 +0600

    dropdown meu with search input completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ import { Link, NavLink } from 'react-router-dom';
 import { CiSearch } from 'react-icons/ci';
 import DropdownButtonLanguage from './UI/DropdownButtonLanguage';
 import DropdownButtonCurrency from './UI/DropdownButtonCurrency';
+import DropdownMenuButton from './UI/DropdownMenuButton';
 // import Dropdown from 'react-bootstrap/Dropdown';
 
 const Header = () => {
@@ -40,18 +41,18 @@ const Header = () => {
                     </div>
             </div>
         </header>
+
         <header className='header-upper py-3'>
             <div className='container-xl'>
                 <div className='row align-items-center'>
                     <div className='col-2'>
-                        <h2 className='text-white'>
-                            <Link className='text-white mb=0'>EDOKAN</Link>
+                        <h2 className='text-white mb-3'>
+                            <Link className='text-white fs-3 fw-bolder'>EDOKAN</Link>
                         </h2>
                     </div>
                     <div className='col-5'>
-                    <div className="input-group mb-3">
-                            <input type="text" className="form-control py-2" placeholder="Search product here..." aria-label="Search product here..." aria-describedby="basic-addon2"/>
-                            <span className="input-group-text p-3 padding" id="basic-addon2"><CiSearch className='fs-4'/></span>
+                        <div className="input-group">
+                            <DropdownMenuButton/>
                         </div>
                     </div>
                     <div className='col-5'>
--- src/components/UI/DropdownMenuButton.js
+++ src/components/UI/DropdownMenuButton.js
@@ -1,16 +1,30 @@
 import React from 'react';
 // import './DropdownMenuButton.css';
+import { CiSearch } from 'react-icons/ci';
+
 
 const DropdownMenuButton = () => {
     return (
-        <div className="dropdown">
-        <select>
-          <option value="option1">Option 1</option>
-          <option value="option2">Option 2</option>
-          <option value="option3">Option 3</option>
-        </select>
-      </div>
+          <div className="dropdown1 d-flex align-items-center">
+              <select>
+                <option value="option1">All Categories</option>
+                <option value="option2">Bangla</option>
+                <option value="option3">Hindi</option>
+              </select>
+              <input type="text" className="form-control py-2" placeholder="Search product here..." aria-label="Search product here..." aria-describedby="basic-addon2"/>
+          <span className="input-group-text p-3 padding" id="basic-addon2"><CiSearch className='fs-5 search'/></span>
+         </div>
     );
 };
 
 export default DropdownMenuButton;
+
+
+
+{/* <div className="dropdown">
+<select>
+  <option value="option1">Option 1</option>
+  <option value="option2">Option 2</option>
+  <option value="option3">Option 3</option>
+</select>
+</div> */}
