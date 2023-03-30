a516ed9c3c7cfd61eecc09d796721d9c31a42c3a
saifulalamfahime <you@example.com>
2023-03-30 23:55:54 +0600

    initial responsive done

--- src/container/AboutContainer.js
+++ src/container/AboutContainer.js
@@ -6,7 +6,7 @@ const AboutContainer = () => {
             <div className='about__text col-8'>
                 <h1>HELLO! <br /> WE'RE <span>EDOKAN</span></h1>
             </div>
-            <div className='about__text col-4'>
+            <div className='about__text col-4 mt-4'>
                 <p>We just moved into the neighborhood, but it's like we've been here forever! You've welcomed us like old friends, dropping by with ready smiles and eager appetites.
                 <br /> <br />
                 That's why we vow to always serve up goodness. Our product will always be organically grown, locally sourced, and served with warm smiles. We're family now. And this is how we make for those we love.
